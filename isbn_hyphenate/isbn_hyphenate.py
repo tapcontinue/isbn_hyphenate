7be08796250eedd885be003e14ceac33f81e19dd
Noel Tanner <noeltanner@gmail.com>
2023-07-09 17:56:06 -0500

    Update isbn_hyphenate.py
    added the ability to hyphenate ISBN-13s that start with 979

--- isbn_hyphenate/isbn_hyphenate.py
+++ isbn_hyphenate/isbn_hyphenate.py
@@ -3,13 +3,13 @@ import re
 import sys
 from . import isbn_lengthmaps
 
-class IsbnError(Exception): 
+class IsbnError(Exception):
     """Base class for exceptions thrown by hyphenate and try_hyphenate."""
     pass
 
 
-class IsbnMalformedError(IsbnError): 
-    """ISBN is malformed, such as too short, too long or 
+class IsbnMalformedError(IsbnError):
+    """ISBN is malformed, such as too short, too long or
     contains invalid characters"""
     pass
 
@@ -21,16 +21,16 @@ class IsbnUnableToHyphenateError(IsbnError):
 
 def hyphenate(input_data):
     """Add hyphens to an International Standard Book Number (ISBN)
-    
+
     >>> hyphenate('1843560283')
     '1-84356-028-3'
-    
+
     10 or 13 digit ISBN are hyphenated. Hyphens and whitespace in the input
     is stipped.
-    
+
     This function uses a database of ISBN ranges, so might not work for
     ISBNs in recently allocated ranges.
-    
+
     Compatible with both Python 2 and 3.
     """
 
@@ -42,47 +42,47 @@ def hyphenate(input_data):
             return_ascii = True
 
     without_hyphens = re.sub('[\s-]', '', input_data)
-    
+
     # Must be digits, maybe an X at the end
     if not re.match('^[0-9]+X?$', without_hyphens):
         raise IsbnMalformedError("Must only contain digits and/or and X")
 
     with_hyphens = ''
-    
+
     GS1_prefix = None
     if len(without_hyphens) == 13:
         GS1_prefix = without_hyphens[:3]
         with_hyphens = GS1_prefix + '-'
         without_hyphens = without_hyphens[3:]
     elif len(without_hyphens) == 10:
-        GS1_prefix = '978'
+        GS1_prefix = '978' if without_hyphens.startswith('978') else '979'
     else:
         raise IsbnMalformedError("Length must be 10 or 13")
-    
+
     first7 = int(without_hyphens[:7])
     group_prefix_length = None
     if GS1_prefix not in isbn_lengthmaps.groups_length:
-        raise IsbnUnableToHyphenateError("GS1 prefix %s not recognized" % 
+        raise IsbnUnableToHyphenateError("GS1 prefix %s not recognized" %
             GS1_prefix)
     for cur_range in isbn_lengthmaps.groups_length[GS1_prefix]:
         if cur_range['min'] <= first7 <= cur_range['max']:
             group_prefix_length = cur_range['length']
             break
-    
+
     if group_prefix_length is None:
         raise IsbnUnableToHyphenateError("Not in any recognized group range")
     elif group_prefix_length == 0:
         raise IsbnUnableToHyphenateError("Group range is unused")
-    
+
     group_prefix = without_hyphens[:group_prefix_length]
     without_hyphens = without_hyphens[group_prefix_length:]
     with_hyphens += group_prefix + '-'
-    
+
     first7 = int(without_hyphens[:7].ljust(7, '0'))
     publisher_length = None
     GS1_and_group = GS1_prefix + '-' + group_prefix
     if GS1_and_group not in isbn_lengthmaps.publisher_length:
-        raise IsbnUnableToHyphenateError("Prefix %s not recognized" % 
+        raise IsbnUnableToHyphenateError("Prefix %s not recognized" %
             GS1_and_group)
     for cur_range in isbn_lengthmaps.publisher_length[GS1_and_group]:
         if cur_range['min'] <= first7 <= cur_range['max']:
@@ -94,22 +94,22 @@ def hyphenate(input_data):
             "Not in any recognized publisher range")
     elif publisher_length == 0:
         raise IsbnUnableToHyphenateError("Publisher range is unused")
-    
+
     publisher = without_hyphens[:publisher_length]
     without_hyphens = without_hyphens[publisher_length:]
     with_hyphens += publisher + '-'
-    
+
     book_id = without_hyphens[:-1]
     check_digit = without_hyphens[-1:]
     with_hyphens += book_id + '-' + check_digit
 
-    # If this is Python 2, and the input was a non-Unicode string, 
+    # If this is Python 2, and the input was a non-Unicode string,
     # then return an ascii string
     if return_ascii:
         with_hyphens = with_hyphens.encode("ascii")
-    
+
     return with_hyphens
-    
+
 
 def try_hyphenate(isbn):
     """Attempts to add hyphens to an International Standard Book Number (ISBN).
@@ -118,6 +118,6 @@ def try_hyphenate(isbn):
     This is a wrapper for hyphenate."""
     try:
         return hyphenate(isbn)
-    except IsbnUnableToHyphenateError:
+    except (IsbnUnableToHyphenateError, IsbnMalformedError):
         return isbn
-        
+
